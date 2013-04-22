5ca7f1af1f017aa896752339f14daa2dba3bad76
Christy Swartz <christy@mit.edu>
2013-04-21 21:36:04 -0400

    init

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, 171110006314660, 3d90204c65fbc578d3805ebae2385657
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Verifiedmail::Application.config.secret_token = '1e500108652f668171ca9f4ddb8b9ef28f41583d0c78d8ed2db897fe464bf06a514dbfbc9cacfe9af46b3a1d51a33741652d4eff8cd51b56e299982d062c9474'
