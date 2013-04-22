d2c4f3532eb7f4576964fe6f7b61154a212bef6d
Christy Swartz <christy@mit.edu>
2013-04-21 23:18:41 -0400

    oauth begin

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, "171110006314660", "3d90204c65fbc578d3805ebae2385657"
+  provider :facebook, 171110006314660, "3d90204c65fbc578d3805ebae2385657" #ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Verifiedmail::Application.routes.draw do
+  resources :users
+
+  resources :mhashes
+
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +57,7 @@ Verifiedmail::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
