75bba74ac8878246cd42055009ba7c9dd2862836
Christy Swartz <christy@mit.edu>
2013-05-13 08:46:11 -0400

    update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,6 @@
 class WelcomeController < ApplicationController
 
   def index
-    current_user = true
     if current_user
       respond_to do |format|
         format.html
@@ -18,7 +17,6 @@ class WelcomeController < ApplicationController
   end
 
   def sign
-    current_user = true
     if current_user
       respond_to do |format|
         format.html
@@ -29,7 +27,6 @@ class WelcomeController < ApplicationController
   end
 
   def verify
-    current_user = true
     if current_user
       respond_to do |format|
         format.html
@@ -40,7 +37,6 @@ class WelcomeController < ApplicationController
   end
 
   def about
-    current_user = true
     if current_user
       respond_to do |format|
         format.html
